78c5397e7172722aac3a228b6a17c886ad188570
Ehsan M.Kermani <ehsanmo1367@gmail.com>
2018-05-29 09:40:48 -0700

    fix doc url

--- src/lib.rs
+++ src/lib.rs
@@ -6,7 +6,7 @@
 // option. This file may not be copied, modified, or distributed
 // except according to those terms.
 #![crate_name = "tvm_rust"]
-#![doc(html_root_url = "https://docs.rs/tvm_rust/0.0.1/")]
+#![doc(html_root_url = "https://docs.rs/tvm-rust/0.0.1/tvm-rust")]
 #![allow(non_camel_case_types, unused_imports, unused_unsafe)]
 
 //! [WIP]
