12053982078b2be2bfa06b19fadb3b0a59fb7bf9
ehsanmok <ehsanmo1367@gmail.com>
2018-08-16 16:22:47 -0400

    complete and cleanup context

--- src/context.rs
+++ src/context.rs
@@ -1,4 +1,13 @@
-use super::*;
+use std::fmt::{self, Formatter, Display};
+use std::os::raw::c_void;
+use std::ptr;
+
+use tvm;
+
+use internal_api;
+use function;
+use TVMValue;
+use TVMResult;
 
 #[derive(Debug, Clone, Copy, Hash, PartialEq, Eq)]
 pub struct TVMDeviceType {
@@ -29,7 +38,20 @@ impl From<tvm::DLDeviceType> for TVMDeviceType {
     }
 }
 
-// TODO: include extTypes
+impl Display for TVMDeviceType {
+    fn fmt(&self, f: &mut Formatter) -> fmt::Result {
+        match self.inner {
+            tvm::DLDeviceType::kDLCPU => write!(f, "cpu"),
+            tvm::DLDeviceType::kDLCPUPinned => write!(f, "cpu_pinned"),
+            tvm::DLDeviceType::kDLGPU => write!(f, "gpu"),
+            tvm::DLDeviceType::kDLOpenCL => write!(f, "opencl"),
+            tvm::DLDeviceType::kDLMetal => write!(f, "meta"),
+            tvm::DLDeviceType::kDLVPI => write!(f, "vpi"),
+            tvm::DLDeviceType::kDLROCM => write!(f, "rocm"),
+        }
+    }
+}
+
 impl<'a> From<&'a str> for TVMDeviceType {
     fn from(type_str: &'a str) -> Self {
         match type_str {
@@ -58,7 +80,16 @@ pub struct TVMContext {
     pub device_id: i32,
 }
 
-// TODO: reexport devs
+macro_rules! impl_context {
+    ($ctx:ident, $dldevt:expr) => {
+        impl TVMContext {
+            pub fn $ctx(device_id: i32) -> Self {
+                Self::new(TVMDeviceType::new($dldevt), device_id)
+            }
+        }
+    }
+}
+
 impl TVMContext {
     pub fn new(device_type: TVMDeviceType, device_id: i32) -> Self {
         TVMContext {
@@ -70,44 +101,38 @@ impl TVMContext {
     pub fn current_context(&self) -> &Self {
         self
     }
+}
 
-    pub fn cpu(device_id: i32) -> Self {
-        TVMContext {
-            device_id: device_id,
-            ..Default::default()
+impl_context!(cpu, tvm::DLDeviceType::kDLCPU);
+impl_context!(cpu_pinned, tvm::DLDeviceType::kDLCPUPinned);
+impl_context!(gpu, tvm::DLDeviceType::kDLGPU);
+impl_context!(cuda, tvm::DLDeviceType::kDLGPU);
+impl_context!(nvptx, tvm::DLDeviceType::kDLGPU);
+impl_context!(cl, tvm::DLDeviceType::kDLOpenCL);
+impl_context!(opencl, tvm::DLDeviceType::kDLOpenCL);
+impl_context!(metal, tvm::DLDeviceType::kDLMetal);
+impl_context!(vpi, tvm::DLDeviceType::kDLVPI);
+impl_context!(rocm, tvm::DLDeviceType::kDLROCM);
+
+macro_rules! impl_dev_attrs {
+    ($attr_name:ident, $attr_kind:expr) => {
+        impl TVMContext {
+            pub fn $attr_name(&self) -> usize {
+                let func = ::internal_api::get_api("_GetDeviceAttr".to_owned());
+                let dt = self.device_type.inner as i32;
+                let ret = function::Builder::from(func)
+                    .push_arg(&dt)
+                    .push_arg(&self.device_id)
+                    .push_arg(&$attr_kind)
+                    .invoke()
+                    .unwrap();
+                unsafe { ret.value.inner.v_int64 as usize }
+            }
         }
-    }
-    // TODO: refactor to macro impl
-    pub fn gpu(device_id: i32) -> Self {
-        Self::new(TVMDeviceType::new(tvm::DLDeviceType::kDLGPU), device_id)
-    }
-
-    pub fn cpu_pinned(device_id: i32) -> Self {
-        Self::new(
-            TVMDeviceType::new(tvm::DLDeviceType::kDLCPUPinned),
-            device_id,
-        )
-    }
-
-    pub fn opencl(device_id: i32) -> Self {
-        Self::new(TVMDeviceType::new(tvm::DLDeviceType::kDLOpenCL), device_id)
-    }
-
-    pub fn metal(device_id: i32) -> Self {
-        Self::new(TVMDeviceType::new(tvm::DLDeviceType::kDLMetal), device_id)
-    }
-
-    pub fn vpi(device_id: i32) -> Self {
-        Self::new(TVMDeviceType::new(tvm::DLDeviceType::kDLVPI), device_id)
-    }
-
-    pub fn rocm(device_id: i32) -> Self {
-        Self::new(TVMDeviceType::new(tvm::DLDeviceType::kDLROCM), device_id)
     }
 }
 
 impl TVMContext {
-    // TODO: macro impl device_api.h
     pub fn exist(&self) -> bool {
         let func = internal_api::get_api("_GetDeviceAttr".to_owned());
         let dt = self.device_type.inner as i32;
@@ -120,32 +145,7 @@ impl TVMContext {
         ret.value != TVMValue::default()
     }
 
-    pub fn max_thread_pre_block(&self) -> usize {
-        let func = internal_api::get_api("_GetDeviceAttr".to_owned());
-        let dt = self.device_type.inner as i32;
-        let ret = function::Builder::from(func)
-            .push_arg(&dt)
-            .push_arg(&self.device_id)
-            .push_arg(&1)
-            .invoke()
-            .unwrap();
-        unsafe { ret.value.inner.v_int64 as usize }
-    }
-
-    pub fn warp_size(&self) -> usize {
-        let func = internal_api::get_api("_GetDeviceAttr".to_owned());
-        let dt = self.device_type.inner as i32;
-        let ret = function::Builder::from(func)
-            .push_arg(&dt)
-            .push_arg(&self.device_id)
-            .push_arg(&2)
-            .invoke()
-            .unwrap();
-        unsafe { ret.value.inner.v_int64 as usize }
-    }
-
     pub fn sync(&self) -> TVMResult<()> {
-        //let handle = ptr::null_mut();
         check_call!(tvm::TVMSynchronize(
             self.device_type.inner as i32,
             self.device_id,
@@ -154,6 +154,15 @@ impl TVMContext {
         Ok(())
     }
 }
+
+impl_dev_attrs!(max_threads_per_block, 1);
+impl_dev_attrs!(warp_size, 2);
+impl_dev_attrs!(max_shared_memory_per_block, 3);
+impl_dev_attrs!(compute_version, 4);
+impl_dev_attrs!(device_name, 5);
+impl_dev_attrs!(max_clock_rate, 6);
+impl_dev_attrs!(multi_processor_count, 7);
+impl_dev_attrs!(max_thread_dimensions, 8);
 
 impl From<TVMContext> for tvm::DLContext {
     fn from(ctx: TVMContext) -> Self {
@@ -175,8 +184,7 @@ impl From<tvm::DLContext> for TVMContext {
 
 impl Display for TVMContext {
     fn fmt(&self, f: &mut Formatter) -> fmt::Result {
-        // TODO: display DeviceType
-        write!(f, "{:?} {}", self.device_type, self.device_id)
+        write!(f, "{}({})", self.device_type, self.device_id)
     }
 }
 
@@ -187,6 +195,7 @@ mod tests {
     #[test]
     fn context() {
         let ctx = TVMContext::cpu(0);
+        println!("ctx: {}", ctx);
         let default_ctx = TVMContext::new(TVMDeviceType::new(tvm::DLDeviceType::kDLCPU), 0);
         assert_eq!(ctx.current_context().clone(), default_ctx);
         assert_ne!(ctx, TVMContext::gpu(0));
@@ -203,10 +212,16 @@ mod tests {
     }
 
     #[test]
-    fn dev_attribute() {
+    fn dev_attributes() {
         let ctx = TVMContext::cpu(0);
         assert!(ctx.exist());
-        println!("max thread per block: {}", ctx.max_thread_pre_block());
+        println!("max thread per block: {}", ctx.max_threads_per_block());
         println!("warp size: {}", ctx.warp_size());
+        println!("max shared memory per block: {}", ctx.max_shared_memory_per_block());
+        println!("compute version: {}", ctx.compute_version());
+        println!("device name: {}", ctx.device_name());
+        println!("max clock rate: {}", ctx.max_clock_rate());
+        println!("multi processor count: {}", ctx.multi_processor_count());
+        println!("max thread dimensions: {}", ctx.max_thread_dimensions());
     }
 }
