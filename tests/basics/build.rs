d20dc18d4ff2b74266b14d27971b51edd0204a44
ehsanmok <ehsanmo1367@gmail.com>
2018-08-17 19:05:00 -0400

    cleaner build

--- tests/basics/build.rs
+++ tests/basics/build.rs
@@ -1,10 +1,10 @@
 use std::process::Command;
 
 fn main() {
-    let mut script_path = concat!(env!("CARGO_MANIFEST_DIR"), "/src/tvm_add_cpu.py");
-    if cfg!(feature = "gpu") {
-        script_path = concat!(env!("CARGO_MANIFEST_DIR"), "/src/tvm_add_gpu.py");
-    }
+    #[cfg(feature = "cpu")]
+    let script_path = concat!(env!("CARGO_MANIFEST_DIR"), "/src/tvm_add_cpu.py");
+    #[cfg(feature = "gpu")]
+    let script_path = concat!(env!("CARGO_MANIFEST_DIR"), "/src/tvm_add_gpu.py");
     let output = Command::new("python")
         .args(&[&script_path, env!("CARGO_MANIFEST_DIR")])
         .output()
